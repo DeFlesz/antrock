2d6e1b337bef31031d89f1490d97ea1ac70c5fb7
DeFlesz <nieflesz@gmail.com>
2024-10-10 13:54:36 +0200

    added glfw as submodule & made sure it works

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <antrock/renderer/renderer.hpp>
+#include <GLFW/glfw3.h>
 
 int main()
 {
 	std::cout << "Hello, world!" << std::endl;
-	renderer r;
-	r.test();
+	//renderer r;
+	//r.test();
+	if (!glfwInit())
+	{
+	    // Initialization failed
+	}
+	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+	if (!window)
+	{
+		// Window or OpenGL context creation failed
+	}
+	while (!glfwWindowShouldClose(window))
+	{
+		// Keep running
+	}
 	return 0;
 }
